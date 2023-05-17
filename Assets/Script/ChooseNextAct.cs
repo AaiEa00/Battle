b33e92ab472d102662723e0967ebfde37f842329
TakamiRena <111954145+AaiEa00@users.noreply.github.com>
2023-05-17 11:01:07 +0900

    battle1_start
    たたかう実装中

---
+++ Assets/Script/ChooseNextAct.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseNextAct : MonoBehaviour
+{
+    [SerializeField] Image image;
+
+    private void Start()
+    {
+        
+    }
+
+    void OnClickTatakau()
+    {
+
+    }
+}
