0bbe2d9fe899df66c6d7fd5bbcb157521f2dc8fa
高見怜那 <111954145+AaiEa00@users.noreply.github.com>
2023-06-07 09:57:13 +0900

    ActSenshi追加
    ChooseNextActionでターン管理しクリックでActSenshiがactiveになる

---
+++ Assets/Script/ChooseNextAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseNextAction : MonoBehaviour
+{
+    [SerializeField] GameObject senshi;
+
+    private void Start()
+    {
+        
+    }
+
+    public void OnClickFight()
+    {
+
+    }
+}
